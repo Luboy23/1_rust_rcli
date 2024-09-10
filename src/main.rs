3c16783f70a877b78616207566a654c4dceee0a9
Luboy23 <126079708+Luboy23@users.noreply.github.com>
2024-09-10 17:17:38 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
-}
+use clap::Parser;
+use anyhow::Result;
+
+
+// rcli csx -i input.csv -0 output.json --header -d ','
+use rcli::{Opts, SubCommand, process_csv};
+
+fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    
+    match opts.cmd{
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output),
+    }
+    }
