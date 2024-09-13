3701a8f7a4129761758b0192c577a93176b03659
unknown <2387443867@qq.com>
2024-09-13 22:44:02 +0800

    finish http sever with axum

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -2,7 +2,7 @@ use tracing::{info, warn};
 use anyhow::Result;
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use axum::{extract::{Path, State}, http::StatusCode, routing::get, Router};
-use tower_http::service::ServeDir;
+use tower_http::services::ServeDir;
 
 #[derive(Debug)]
 struct HttpServeState {
@@ -12,19 +12,14 @@ struct HttpServeState {
 pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0,0,0,0], port));
     info!("Serving {:?} on  {}", path, addr);
-    let state = HttpServeState{ path.clone()};
-
-    let dir_servie = ServeDir::new()
-    .append_index_html_if_directory(true)
-    .precompressed_gzip()
-    .precompressed_br()
-    .precompressed_delete();
-
+    let state = HttpServeState{  path:path.clone() };
     // axum router
-    let router = Router::new().route("/*path", get(file_handler))
-    .route_service("/tower", ServiceDir::new(path))
-  
+    let router = Router::new()
+    .nest_service("/tower", ServeDir::new(path))
+    .route("/*path", get
+    (file_handler))
     .with_state(Arc::new(state));
+
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
     Ok(()) 
@@ -44,8 +39,24 @@ async fn file_handler( State(state): State<Arc<HttpServeState>>, Path(path): Pat
         }
         Err(e) => {
             warn!("Error reading dile: {:?}", e);
-            (StatusCode:: INTERNAL_SERVER_ERROR, e.to_string())
+            (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
             }
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path:PathBuf::from("."),
+        });
+        let (status, content) = 
+        file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
+        }
+}
+
